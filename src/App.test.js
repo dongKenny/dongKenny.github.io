e54cb8fc3a1c89113d7a1adf4e1d576d620cc356
dongKenny <kdong4@dons.usfca.edu>
2024-02-05 15:58:35 -0800

    Fixed test cast

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,13 @@
 import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+test('renders 日本研究', () => {
+  render(
+  	<BrowserRouter>
+		  <App />
+	  </BrowserRouter>,
+  );
+  const linkElement = screen.getByText(/日本研究/i);
   expect(linkElement).toBeInTheDocument();
 });
