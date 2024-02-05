fc2f85cf44d5a4635ef65a5e057e78e2554aeb07
dongKenny <kdong4@dons.usfca.edu>
2024-02-05 15:32:03 -0800

    Second Deploy

---
+++ src/components/Home/index.js
@@ -0,0 +1,24 @@
+import './index.scss';
+
+const Home = () => {
+	return (
+		<>
+			<div className="container home-page">
+				<div className="text-zone">
+					<h1>
+						<span>Hi,</span>
+						<br />
+						<span>I'm Kenny.</span>
+						<br />
+						<br />
+						<span className="titles">BA 日本研究</span>
+						<br />
+						<span className="titles">MS Computer Science</span>
+					</h1>
+				</div>
+			</div>
+		</>
+	)
+}
+
+export default Home
