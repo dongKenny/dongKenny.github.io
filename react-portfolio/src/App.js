091e09c32839d0493394428a914872073eb6da85
dongKenny <kdong4@dons.usfca.edu>
2024-02-05 15:24:35 -0800

    First deploy

---
+++ react-portfolio/src/App.js
@@ -0,0 +1,21 @@
+import { Route, Routes } from 'react-router-dom'
+import Home from './components/Home'
+import About from './components/About'
+import Layout from './components/Layout'
+import './App.scss'
+
+function App() {
+	return (
+		<>
+			<Routes>
+				<Route path="/" element={<Layout />}>
+					<Route index element={<Home />} />
+					<Route path="about" element={<About />} />
+				</Route>
+			</Routes>
+		</>
+	)
+}
+
+
+export default App;
